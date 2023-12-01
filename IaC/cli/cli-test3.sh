947eb51eb5bffc600869705649645e2914b8ba6d
Roger <roeschu.bluem@gmail.com>
2023-12-01 23:38:07 +0100

    bin am Verzweiflen mit dem deployment für codebild

--- IaC/cli/cli-test3.sh
+++ IaC/cli/cli-test3.sh
@@ -9,4 +9,19 @@ BATCH_PROJECT="github-to-lambda"
 
 
 
-aws codebuild create-project --cli-input-json file://lambda_project_build.json
+#aws codebuild create-project --cli-input-json file://lambda_project_build.json
+#aws codebuild batch-get-projects --names testen > testen.json
+#aws codebuild create-project --generate-cli-skeleton > test-2.json
+#aws codebuild create-project --cli-input-json file://test-2.json
+
+#aws s3api list-buckets --query "Buckets[].Name" --region eu-central-2
+aws s3api list-objects --bucket mysembucketinput --query "Contents[].Key" --output text --region eu-central-2
+aws s3api list-buckets
+
+
+
+aws codebuild create-project --cli-input-json file://test-2.json --region eu-central-2
+
+    
+
+
