7c5f9fa69db81350bd71136e1808b4e8d982b7bf
Roger <roeschu.bluem@gmail.com>
2023-12-12 20:35:05 +0100

    Test Deployment

--- SemLambdaFunction.py
+++ SemLambdaFunction.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda -  !!! TEST-4444 !!!')
+        'body': json.dumps('Hello from Lambda -  !!! TEST-5555 !!!')
     }
