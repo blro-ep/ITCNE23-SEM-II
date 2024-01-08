41f2dc694d0668d94d8075a559012f6702abc8ec
Roger <126095018+blro-ep@users.noreply.github.com>
2024-01-08 20:15:06 +0100

    Update SemLambdaFunction.py

--- SemLambdaFunction.py
+++ SemLambdaFunction.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda -  !!! TEST-demo-9 !!!')
+        'body': json.dumps('Hello from Lambda -  !!! TEST-demo-10 !!!')
     }
