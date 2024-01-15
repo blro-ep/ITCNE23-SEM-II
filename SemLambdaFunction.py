2664da333cd687e199b3419120423a47cf107f2b
Roger <roeschu.bluem@gmail.com>
2024-01-15 21:25:21 +0100

    testing

--- SemLambdaFunction.py
+++ SemLambdaFunction.py
@@ -3,12 +3,14 @@ import json
         
 def lambda_handler(event, context):
     json_region = os.environ['AWS_REGION']
+    json_function_name = os.environ['AWS_LAMBDA_FUNCTION_NAME']
     return {
         "statusCode": 200,
         "headers": {
             "Content-Type": "application/json"
         },
         "body": json.dumps({
-            "Region ": json_region
+            "Region ": json_region,
+            "Function Name ": json_function_name
         })
     }
