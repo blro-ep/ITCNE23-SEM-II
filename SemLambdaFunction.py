a879d7bebd73eb755a6b9014608c2b4b7580eb02
Roger <roeschu.bluem@gmail.com>
2024-01-15 22:30:52 +0100

    testing

--- SemLambdaFunction.py
+++ SemLambdaFunction.py
@@ -3,7 +3,6 @@ import json
         
 def lambda_handler(event, context):
     json_region = os.environ['AWS_REGION']
-    json_tz = os.environ['TZ']
     json_function_name = os.environ['AWS_LAMBDA_FUNCTION_NAME']
     json_function_memory = os.environ['AWS_LAMBDA_FUNCTION_MEMORY_SIZ']
     json_function_version = os.environ['AWS_LAMBDA_FUNCTION_VERSION']
@@ -15,7 +14,6 @@ def lambda_handler(event, context):
         },
         "body": json.dumps({
             "Region ": json_region,
-            "Time Zone ": json_tz,
             "Lambda Function Name ": json_function_name,
             "Lambda Function Memory size ": json_function_memory,
             "Lambda Function Version ": json_function_version
